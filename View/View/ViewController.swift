b402a40efbbead503f9e8651aecaf67b8c5ec233
kaganskaya <lizkaganskaya@gmail.com>
2019-02-10 13:28:07 +0200

    View project add

---
+++ View/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  View
+//
+//  Created by liza_kaganskaya on 2/10/19.
+//  Copyright © 2019 liza_kaganskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
